9f5714170f7b954d33f595ea17fd3c53e2d89dd2
Andreyjkov <Andreyjskov@gmail.com>
2025-05-17 21:04:17 +0300

    feat: info update

--- js/language.js
+++ js/language.js
@@ -1,25 +1,25 @@
 const language = {
-  "h1-title": {
-    ru: "АНДРЕЙ КОВАЛЬ",
-    en: "ANDREY KOVAL",
+  'h1-title': {
+    ru: 'АНДРЕЙ КОВАЛЬ',
+    en: 'ANDREY KOVAL',
   },
-  "h1-subtitle": {
-    ru: "Веб-разработчик",
-    en: "Web Developer",
+  'h1-subtitle': {
+    ru: 'Веб-разработчик',
+    en: 'Web Developer',
   },
   contact: {
-    ru: "Контакты",
-    en: "Contacts",
+    ru: 'Контакты',
+    en: 'Contacts',
   },
   location: {
-    ru: "Гродно, Беларусь",
-    en: "Grodno,Belarus",
+    ru: 'Гродно, Беларусь',
+    en: 'Grodno,Belarus',
   },
   about: {
-    ru: "Обо мне",
-    en: "about me",
+    ru: 'Обо мне',
+    en: 'about me',
   },
-  "about-text": {
+  'about-text': {
     ru: `Более десяти лет занимался ремонтом мобильных телефонов, ноутбуков и прочей
     техники. <br>
     Мои личные качества: трудолюбие, терпение, надежность, дисциплинированность, честность, дружелюбие. <br>
@@ -30,14 +30,14 @@ const language = {
     I want to work in a team with professionals to improve my skills in programming and career growth.`,
   },
   exp: {
-    ru: "Опыт",
-    en: "experience",
+    ru: 'Опыт',
+    en: 'experience',
   },
   skills: {
-    ru: "Навыки",
-    en: "Skills",
+    ru: 'Навыки',
+    en: 'Skills',
   },
-  "htnl-css3": {
+  'htnl-css3': {
     ru: `- Учебный проект <a
     href="https://rolling-scopes-school.github.io/andreyjkov-JSFE2022Q1/shelter/pages/main/index.html"
     target="_blank">Shelter</a>
@@ -72,9 +72,9 @@ const language = {
   - Study project in a team <a href="https://sergeymikhalkin.github.io/rslang/" target="_blank">Rs
     lang</a> - an application for learning foreign words, including an electronic textbook with a database of words for learning, mini-games for repeating them, a statistics page for tracking individual progress`,
   },
-  "type-script": {
-    ru: "В учебных заданиях работал с TypeScript",
-    en: "Worked with TypeScript in tutorial assignments",
+  'type-script': {
+    ru: 'В учебных заданиях работал с TypeScript',
+    en: 'Worked with TypeScript in tutorial assignments',
   },
   react: {
     ru: `- Учебный проект в команде <a
@@ -85,62 +85,115 @@ const language = {
     href="https://project-management-app-22.vercel.app/">Project
     management</a> - is an application that helps a team or group of developers achieve their goals.`,
   },
+  reactNative: {
+    ru: '- приложения женский календарь',
+    en: "women's calendar apps",
+  },
+  calendarTitle: {
+    ru: 'Приложения женский календарь',
+    en: "Women's calendar apps",
+  },
+  calendarSubtitle: {
+    ru: 'Комерческое приложения женский календарь - Отслеживает менструацию, прогнозирует цикл, колесо фертильности - рассчитывает вероятность зачатия ребенка, готовит к зачатию, напоминает о приеме контрацепции и лекарств, имеет функцию виртуальной медицинской карты - хранение данных УЗИ-диагностики, анализов, консультативных заключений. . Индивидуальный датчик для диагностики мышц тазового дна. Техники медитации для психического здоровья. AI-поддержка в любое время суток. Доступ к приложению партнеру - партнер видит настроение и самочувствие партнера. Курс по укреплению мышц тазового дна. Консультации гинекологов и сексологов.',
+    en: 'It tracks menstruation, predicts the cycle, fertility wheel - calculates the probability of conceiving a child, prepares for conception, reminds you to take contraception and medicines, has the function of a virtual medical card - storage of ultrasound diagnostic data, analyzes, advisory opinions. Individual sensor for diagnosing pelvic floor muscles. Meditation techniques for mental health. AI support at any time of the day. Access to the application to the partner - the partner sees the mood and well-being of the partner. A course on strengthening the muscles of the pelvic floor. Consultations of gynecologists and sexologists.',
+  },
   redux: {
-    ru: "- хорошо разобрался в работе Redux-toolkit",
-    en: "well versed in the work of Redux-toolkit",
+    ru: '- хорошо разобрался в работе Redux-toolkit',
+    en: 'well versed in the work of Redux-toolkit',
   },
   git: {
-    ru: "На протяжении всего обучения в The Rolling Scopes я работал с Git.",
-    en: "Throughout my training at The Rolling Scopes, I worked with Git.",
+    ru: 'На протяжении всего обучения в The Rolling Scopes я работал с Git.',
+    en: 'Throughout my training at The Rolling Scopes, I worked with Git.',
   },
   bootstrap: {
-    ru: "Ознакомился при написании данной CV",
-    en: "I met while writing this CV.",
+    ru: 'Ознакомился при написании данной CV',
+    en: 'I met while writing this CV.',
   },
   education: {
-    ru: "Образование",
-    en: "Education",
+    ru: 'Образование',
+    en: 'Education',
   },
-  "rss-app-school": {
-    ru: "Бесплатная образовательная программа для сообщества, проводимая сообществом разработчиков The Rolling Scopes. с 2013 года.",
-    en: "RS School is free-of-charge and community-based education program conducted by The Rolling Scopes developer community since 2013.",
+  'rss-app-school': {
+    ru: 'Бесплатная образовательная программа для сообщества, проводимая сообществом разработчиков The Rolling Scopes. с 2013 года.',
+    en: 'RS School is free-of-charge and community-based education program conducted by The Rolling Scopes developer community since 2013.',
   },
   work: {
-    ru: "Мастер по ремонту электроники",
-    en: "Electronics repairman",
+    ru: 'Мастер по ремонту электроники',
+    en: 'Electronics repairman',
   },
   english: {
-    ru: "Английский",
-    en: "English",
+    ru: 'Английский',
+    en: 'English',
   },
-  "lvl-english": {
-    ru: "A1-A2 - Сейчас в активном обучении",
-    en: "A1-A2 - Currently in active learning",
+  'lvl-english': {
+    ru: 'A1-A2 - Сейчас в активном обучении',
+    en: 'A1-A2 - Currently in active learning',
   },
   college: {
-    ru: "Колледж",
-    en: "College",
+    ru: 'Колледж',
+    en: 'College',
   },
-  "college-text": {
-    ru: "Гродненский государственный профессионально-технический колледж приборостроения. Специальность - Ремонт и техническое обслуживание телерадиоаппаратуры",
-    en: "Grodno State Vocational College of Instrument Engineering. Speciality - Repair and maintenance of television radio equipment",
+  'college-text': {
+    ru: 'Гродненский государственный профессионально-технический колледж приборостроения. Специальность - Ремонт и техническое обслуживание телерадиоаппаратуры',
+    en: 'Grodno State Vocational College of Instrument Engineering. Speciality - Repair and maintenance of television radio equipment',
   },
-  "accordion-3": {
-    ru: "Пет-проекты",
-    en: "Pet projects",
+  'accordion-3': {
+    ru: 'Опыт работы',
+    en: 'Work Experience',
   },
-  "second-hand-parts": {
-    ru: "shp",
-    en: "shp",
+  'second-hand-parts': {
+    ru: 'shp',
+    en: 'shp',
   },
-  "shp-text": {
+  'shp-text': {
     ru: `В разработке <a href='https://second-hand-parts-870e7.web.app' target="_blank">Second Hand Parts</a> - сайт для поиска и продажи подержанных запчастей.<br>
     - Код <a href="https://github.com/Andreyjkov/secondHandParts/tree/develop" target="_blank">Second Hand Parts</a>`,
     en: `In developing <a href='https://second-hand-parts-870e7.web.app' target="_blank">Second Hand Parts</a> - site for finding and selling used parts. <br>
     - Code <a href="https://github.com/Andreyjkov/secondHandParts/tree/develop" target="_blank">Second Hand Parts</a>`,
   },
-  "my-name": {
-    ru: "Андрей Коваль",
-    en: "Andrey Koval",
+  'my-name': {
+    ru: 'Андрей Коваль',
+    en: 'Andrey Koval',
+  },
+  'teamwork-title': {
+    ru: 'Коммерческий командный проект',
+    en: 'Commercial Team Project',
+  },
+  'teamwork-text': {
+    ru: '- Разработка адаптивного веб-сайта с широким функционалом для обмена и стейкинга криптовалют',
+    en: '- Developed a responsive website with extensive functionality for crypto exchange and staking',
+  },
+  'teamwork-text-1': {
+    ru: '- Потдержка мобильного приложения с использованием React Native',
+    en: '- Maintained a mobile application using React Native',
+  },
+  'teamwork-text-2': {
+    ru: '- Работа с REST API и интеграцией с бэкендом',
+    en: '- Worked with REST APIs and backend integration',
+  },
+  'teamwork-text-3': {
+    ru: '- Активное участие в командной разработке (дизайн, разработка, тестирование)',
+    en: '- Actively participated in team-based development (design, coding, and testing)',
+  },
+  'teamwork-text-4': {
+    ru: '- Получил ценный практический опыт в командной работе и взаимодействии с другими специалистами',
+    en: '- Gained valuable hands-on experience in teamwork and cross-functional collaboration',
+  },
+  'teamwork-text-5': {
+    ru: '- Улучшил навыки вёрстки и построения сложных адаптивных интерфейсов',
+    en: '- Improved skills in layout and building complex responsive interfaces',
   },
 };
+/* 
+Потдержка мобильного приложения с использованием React Native
+
+Разработка административной панели для управления платформой
+
+Работа с REST API и интеграцией с бэкендом
+
+Активное участие в командной разработке (дизайн, разработка, тестирование)
+
+Получил ценный практический опыт в командной работе и взаимодействии с другими специалистами
+
+Улучшил навыки вёрстки и построения сложных адаптивных интерфейсов
+*/
